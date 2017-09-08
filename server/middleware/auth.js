57bd2a9e9321607e1713eedf1d3c3b381e0bd337
Steve Kuo <stevepkuo@gmail.com>
2017-09-07 22:43:51 -0700

    Add CRUD server routes for Boards. Add auth tests for Boards server route. Add middleware authorization checks to make sure user is authorized to access Boards server routes

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,12 +1,96 @@
 const session = require('express-session');
 const RedisStore = require('connect-redis')(session);
 const redisClient = require('redis').createClient();
+const models = require('../../db/models');
+
+//for testing purposes to set up a fake login session
+module.exports.fakemiddleware = (req, res, next) => {
+  if (req && req.session && req.session.user_tmp) {
+    req.user = req.session.user_tmp;
+  }
+  if (next) {
+    next();
+  }
+};
 
 module.exports.verify = (req, res, next) => {
   if (req.isAuthenticated()) {
     return next();
   }
   res.redirect('/login');
+};
+
+module.exports.verifyElse401 = (req, res, next) => {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.sendStatus(401);
+};
+
+module.exports.verifyBoardMemberElse401 = (req, res, next) => {
+  var boardid;
+  if (!req.params && !req.body) {
+    res.status(400).send('board id couldnt be found in request from client');
+  }
+  if (!req.params.id) {
+    boardid = parseInt(req.body.id);
+  } else {
+    boardid = parseInt(req.params.id);
+  }
+  var userId = req.user.id;
+
+  // return models.User.where({ id: userId }).fetch({withRelated:['memberOfBoards']})
+  // .then(function(user) {
+  //   return user.related('memberOfBoards');
+  // })
+
+  //see whether the boardid shows up under any of the users boards
+  return models.User.where({ id: userId }).fetch()
+    .then(function(user) {
+      return user.related('memberOfBoards').fetch();
+    })
+    .then(boards => {
+      return boards.toJSON().filter((eachBoard) => {
+        return eachBoard.id === boardid;
+      });
+    })
+
+    .then(function(results) {
+      if (results.length === 0) {
+        res.status(401).send();
+      }
+      return next();
+    })
+    .error(err => {
+      res.status(500).send();
+    });
+};
+
+module.exports.verifyBoardOwnerElse401 = (req, res, next) => {
+  var boardid;
+  if (!req.params && !req.body) {
+    res.status(400).send('board id couldnt be found in request from client');
+  }
+  if (!req.params.id) {
+    boardid = parseInt(req.body.id);
+  } else {
+    boardid = parseInt(req.params.id);
+  }
+  var userId = req.user.id;
+  //see whether the boardid's shows up under any of the users boards
+  return models.Board.where({ id: boardid }).fetch()
+    .then(function(board) {
+      return board.toJSON().owner_id === userId;
+    })
+    .then(ownsBoard => {
+      if (!ownsBoard) {
+        res.status(401).send();
+      }
+      return next();
+    })
+    .error(err => {
+      res.status(500).send();
+    });
 };
 
 module.exports.session = session({
--- server/middleware/passport.js
+++ server/middleware/passport.js
@@ -16,7 +16,8 @@ passport.deserializeUser((user, done) => {
 passport.use('github', new GithubStrategy({
   clientID: config.Github.clientID,
   clientSecret: config.Github.clientSecret,
-  callbackURL: config.Github.callbackURL
+  callbackURL: config.Github.callbackURL,
+  scope: 'user:email' //get github email
 },
 (accessToken, refreshToken, profile, done) => getOrCreateGithubOAuthProfile('github', profile, done))
 );
@@ -28,23 +29,31 @@ const getOrCreateGithubOAuthProfile = (type, oauthProfile, done) => {
       //check if Auth table already has user
       if (oauthAccount) {
         console.log('User table already has the Github user');
-        throw oauthAccount;
+        console.log(oauthAccount);
+      } else {
+        console.log('Github User not found in User table');
+        console.log(oauthProfile);
       }
-      console.log('Github User not found in User table');
-      console.log(oauthProfile);
 
+      var email = null;
+      oauthProfile.emails.forEach((eachEmail) => {
+        if (!!eachEmail.primary) {
+          email = eachEmail.value;
+        }
+      });
       let profileInfo = {
         github_handle: oauthProfile.username,
         profile_photo: oauthProfile.photos[0].value,
-        oauth_id: oauthProfile.id
+        oauth_id: oauthProfile.id,
+        email: email
       };
-      return models.User.forge(profileInfo).save();
-    })
-    .catch(oauthAccount => {
-      if (!oauthAccount.get) {
-        throw oauthAccount;
+
+      if (oauthAccount) {
+        //update profile with info from oauthProfile
+        return oauthAccount.save(profileInfo, { method: 'update' });
       }
-      return oauthAccount;
+      //otherwise create new profile
+      return models.User.forge(profileInfo).save();
     })
     .then(profile => {
       if (profile) {
